5d99b46d9a8fa93376e6481dc189bd77afe02cc5
Anthony Salamanca <salamanca@cityinspain.dev>
2022-03-04 19:20:33 -0500

    implement positioned yellow inputs

--- src/App.tsx
+++ src/App.tsx
@@ -36,11 +36,42 @@ function App() {
     4: "",
   });
 
+  const [positionedYellowLetters, setPositionedYellowLetters] = useState({
+    0: "",
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+  });
+
   // TODO: prevent duplicate entries
   const [yellowLetters, setYellowLetters] = useState("");
   const [grayLetters, setGrayLetters] = useState("");
 
   const [words, setWords] = useState(wordList);
+
+  const onResetApp = (e) => {
+    setGreenLetters({
+      0: "",
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+    });
+
+    setPositionedYellowLetters({
+      0: "",
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+    });
+
+    setYellowLetters("");
+
+    setGrayLetters("");
+  };
+
   const handleChange = (e) => {
     const key = e.target.name;
     const newPair = { [key]: e.target.value.toLowerCase() };
@@ -49,6 +80,11 @@ function App() {
 
   const handleYellowLettersChange = (e) => setYellowLetters(e.target.value);
   const handleGrayLettersChange = (e) => setGrayLetters(e.target.value);
+  const handlePositionedYellowChange = (e) => {
+    const key = e.target.name;
+    const newPair = { [key]: e.target.value.toLowerCase() };
+    setPositionedYellowLetters({ ...positionedYellowLetters, ...newPair });
+  };
 
   useEffect(() => {
     const createGreenLetterRegex = (inputState) => {
@@ -60,46 +96,79 @@ function App() {
       );
     };
 
+    const createPositionedYellowLetterRegex = (inputState) => {
+      return new RegExp(
+        "^" +
+          Object.values(inputState)
+            .map((i: string) => (i.length > 0 ? `[^${i}]` : "."))
+            .join("")
+      );
+    };
+
     const wordContainsYellows = (word: string) =>
       yellowLetters.split("").every((letter) => word.includes(letter));
     const wordDoesNotContainGrays = (word: string) =>
       grayLetters.split("").every((letter) => !word.includes(letter));
 
+    const wordContainsAllPositionedYellows = (word: string) =>
+      // could also use a nested .every()
+      Object.values(positionedYellowLetters)
+        .join("") // combine all parts into one big string
+        .split("") // each letter in our combined string
+        .every((letter) => word.includes(letter));
     const greenLetterRegex = createGreenLetterRegex(greenLetters);
+    const positionedYellowLetterRegex = createPositionedYellowLetterRegex(
+      positionedYellowLetters
+    );
 
     setWords(
       wordList.filter(
         (word) =>
           word.match(greenLetterRegex) &&
-          wordContainsYellows(word) &&
+          word.match(positionedYellowLetterRegex) &&
+          wordContainsAllPositionedYellows(word) &&
           wordDoesNotContainGrays(word)
       )
     );
-  }, [greenLetters, yellowLetters, grayLetters]);
+  }, [greenLetters, yellowLetters, grayLetters, positionedYellowLetters]);
 
   return (
     <>
       <div className="container">
         <div className="controls">
-          <div className="green-letters-container">
-            {Object.keys(greenLetters).map((input) => (
-              <input
-                className="green-letter-field"
-                maxLength={1}
-                value={greenLetters[input]}
-                onChange={handleChange}
-                name={input}
-              ></input>
-            ))}
+          <h1 style={{ textAlign: "center", marginBottom: "50px" }}>cheadle</h1>
+          <div className="green-letter-wrapper">
+            <h2 style={{ color: "green", textAlign: "center" }}>GREEN</h2>
+            <div className="green-letters-container">
+              {Object.keys(greenLetters).map((input) => (
+                <input
+                  className="green-letter-field"
+                  maxLength={1}
+                  value={greenLetters[input]}
+                  onChange={handleChange}
+                  name={input}
+                ></input>
+              ))}
+            </div>
           </div>
-          <div className="text-area-container">
+
+          <div className="positioned-yellow-wrapper">
             <h2 style={{ color: "#F1C40F" }}>YELLOW</h2>
-            <textarea
-              className="yellow-letter-textarea"
-              value={yellowLetters}
-              onChange={handleYellowLettersChange}
-            ></textarea>
+            <div className="positioned-yellow-letters-container">
+              {Object.keys(positionedYellowLetters).map((input) => (
+                <textarea
+                  className="positioned-yellow-letter-field"
+                  style={{ height: "150px", width: "20px" }}
+                  value={positionedYellowLetters[input]}
+                  onChange={handlePositionedYellowChange}
+                  name={input}
+                  wrap="physical"
+                  cols={1}
+                ></textarea>
+              ))}
+            </div>
           </div>
+
           <div className="text-area-container">
             <h2 style={{ color: "gray" }}>GRAY</h2>
             <textarea
@@ -108,12 +177,17 @@ function App() {
               onChange={handleGrayLettersChange}
             ></textarea>
           </div>
+          <div className="reset-button-container">
+            <button onClick={onResetApp}>reset</button>
+          </div>
         </div>
         <div
           style={{
             height: "100vh",
             overflowY: "scroll",
             flex: "1",
+            background: "#272729",
+            color: "white",
           }}
         >
           <ul className="word-list">
